54cf1b84cc26581cead23fc9d4b8f35ff577efd7
Anton Semjonov <anton@semjonov.de>
2019-04-20 03:41:33 +0200

    implement direction translation with octal format strings

--- randomart.py
+++ randomart.py
@@ -7,42 +7,31 @@ from randomart import metadata
 from numpy import zeros, array
 from hashlib import blake2b
 
-# split every 3 bytes
-def split3(bytearr):
-  if len(bytearr) % 3 != 0:
-    raise ValueError("length of 'bytearr' not divisible by 3")
-  return (bytearr[i : i + 3] for i in range(0, len(bytearr), 3))
+def bytes_to_octal(bytestr):
+  # calculate best possible number of digits
+  digits = int(len(bytestr) * 8/3)
+  # generate format string
+  fmt = b"%%0%do" % digits
+  # convert bytes to int and format as octal string
+  octal = fmt % int.from_bytes(bytestr, byteorder='big')
+  # return maximum "saturated" digits
+  return octal[-digits:]
 
-# convert three bytes to a large int and
-# split that into three-bit values {0..7}
-def bits(triplet):
-  if len(triplet) != 3:
-    raise ValueError("length of 'triplet' not 3")
-  concat = (triplet[0] << 16) + (triplet[1] << 8) + triplet[2]
-  return [concat >> sh & 7 for sh in range(0, 24, 3)]
-
-# associate values {0..7} with matrix movements
-directions = {
-    4:(-1,-1), 5:(-1, 0), 6:(-1, 1),
-    3:( 0,-1),            7:( 0, 1),
-    2:( 1,-1), 1:( 1, 0), 0:( 1, 1),
+def directions(bytestr):
+  # associate octal digit with movement vector
+  dirhash = {
+      ord('4'):(-1,-1), ord('5'):(-1, 0), ord('6'):(-1, 1),
+      ord('3'):( 0,-1),                   ord('7'):( 0, 1),
+      ord('2'):( 1,-1), ord('1'):( 1, 0), ord('0'):( 1, 1),
   }
+  # return generator mapping each digit
+  return (dirhash[c] for c in bytestr)
 
-# get a generator of movements in matrix
-def movements(H):
-  return (directions[i] for l in (bits(t) for t in split3(H)) for i in l)
-
-# must follow hashlib library api and produce 64+ byte digest
-HASH = blake2b
-
-# read data in chunks
-CHUNKSIZE = 64 * 1024
-
-# digest a reader in chunks to produce pseudorandom data
+# update hash in chunks and digest
 def digest(reader):
-  h = HASH()
+  h = blake2b()
   while True:
-    buf = reader.read(4096)
+    buf = reader.read(64 * 1024)
     if len(buf) == 0:
       break
     h.update(buf)
@@ -55,7 +44,7 @@ def randomart(H):
   mat = zeros(size).astype(int)
   pos = (array(size) / 2).astype(int)
   # perform movements and compute matrix
-  for mov in movements(H):
+  for mov in directions(bytes_to_octal(H)):
     p = tuple(pos)
     mat.itemset(p, mat.item(p) + 1)
     pos = (pos + mov) % size
@@ -114,4 +103,4 @@ if __name__ == "__main__":
 
   if args.hash:
     printhash(d)
-  draw(randomart(d[:63]), args.ascii)
+  draw(randomart(d), args.ascii)
